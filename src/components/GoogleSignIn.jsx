5ee8b804bc0db33671b7c947d78584c94dfa8794
noboKumar <nobokumar227@gmail.com>
2025-08-24 03:07:33 +0600

    navigate user after google login

--- src/components/GoogleSignIn.jsx
+++ src/components/GoogleSignIn.jsx
@@ -1,14 +1,17 @@
 import React, { useContext } from "react";
 import { FcGoogle } from "react-icons/fc";
 import { AuthContext } from "../context/AuthContext";
+import { useNavigate } from "react-router";
 
 const GoogleSignIn = () => {
   const { googleSignIn } = useContext(AuthContext);
-  
+  const navigate = useNavigate();
+
   const handleSignIn = () => {
     googleSignIn()
       .then((result) => {
         console.log(result);
+        navigate("/");
       })
       .catch((error) => {
         console.error(error);
